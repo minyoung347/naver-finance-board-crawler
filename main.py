05da93185680e5ea8d1ae66b0f3f091ee586d8af
gyusu <slhopy@gmail.com>
2020-01-17 22:47:36 +0900

    main args 추가
    main args에 단일 종목 크롤링 추가

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ from crawler import Crawler
 def parse_args():
     parser = argparse.ArgumentParser(description="네이버 금융 종목 토론실 크롤러")
     parser.add_argument('-n', '--n_process', help="병렬 프로세스 개수", type=int, default=os.cpu_count())
+    parser.add_argument('-code', '--code', help="종목코드. 미설정시 전 종목 크롤링", type=str, default=None)
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
     naver_crawler = Crawler(args.n_process)
-    naver_crawler.fetch_all()
+    if args.code:
+        naver_crawler.fetch_one(args.code)
+    else:
+        naver_crawler.fetch_all()
 
 
 if __name__ == '__main__':
